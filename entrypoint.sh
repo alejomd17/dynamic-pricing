6dff0fec102e536a81797efd43d14c62a6bef5ba
Alejandro Moscoso Deossa <alejo-mdm@hotmail.com>
2026-06-12 18:32:44 -0500

    Fix Next.js port conflict with Render

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ uvicorn main:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ip
 
 echo "Iniciando Frontend (Next.js)"
 cd /app/frontend
-HOSTNAME="0.0.0.0" node server.js &
+HOSTNAME="0.0.0.0" PORT=3000 node server.js &
 
 echo "Iniciando Nginx"
 nginx -g "daemon off;"
